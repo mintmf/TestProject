47ef89c92cd4192c24fca4bbb45d05da35c5c050
mint_mf <31040139+mintmf@users.noreply.github.com>
2022-04-12 21:11:45 +0300

    Delete TestProjectDatabase directory

--- TestProjectDatabase/dbo/Tables/Orders.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [dbo].[Orders] (
-    [id]          INT   IDENTITY (1, 1) NOT NULL,
-    [order_date]  DATE  NOT NULL,
-    [order_sum]   MONEY NOT NULL,
-    [payment_sum] MONEY NOT NULL,
-    CONSTRAINT [PK_Orders] PRIMARY KEY CLUSTERED ([id] ASC)
-);
-
